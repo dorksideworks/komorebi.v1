4d613e6f6b86edd047086396f14282fda3dc30e6
komorebi <adrianraycasas@gmail.com>
2024-06-02 03:07:15 +0400

    rounded footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div class="bg-komorebi-dark-3 py-10 flex flex-row justify-around align-around">
+    <div class="bg-komorebi-dark-3 py-10 flex flex-row justify-around align-around rounded-footer">
         <div class="text-gray-500 font-bold">© 2024 ADRIAN RAY CASAS</div>
       
     </div>
